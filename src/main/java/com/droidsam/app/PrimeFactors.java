bd3b7f49a841c159402879e3c44236e5dad7673e
droidsam1 <droidsam@gmail.com>
2022-10-21 20:09:25 +0200

    refactor: reduce duplicity by using tail recursion

--- src/main/java/com/droidsam/app/PrimeFactors.java
+++ src/main/java/com/droidsam/app/PrimeFactors.java
@@ -3,18 +3,17 @@ package com.droidsam.app;
 public class PrimeFactors {
     public static int[] of(int input) {
 
-        if (input == 8) {
-            return new int[]{2, 2, 2};
-        }
-
-        if (input == 6) {
-            return new int[]{2, 3};
-        }
-
-        if (input == 4) {
-            return new int[]{2, 2};
+        if (input % 2 == 0 && input > 2) {
+            return join(new int[]{2}, of(input / 2));
         }
 
         return new int[]{input};
+    }
+
+    private static int[] join(int[] array1, int[] array2) {
+        int[] result = new int[array1.length + array2.length];
+        System.arraycopy(array1, 0, result, 0, array1.length);
+        System.arraycopy(array2, 0, result, array1.length, array2.length);
+        return result;
     }
 }
