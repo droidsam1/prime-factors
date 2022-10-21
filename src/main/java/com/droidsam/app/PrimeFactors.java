fc0ad3ff80b50d3573aa45985abd2d1aaea68b73
droidsam1 <droidsam@gmail.com>
2022-10-21 19:56:12 +0200

    add prime factors for 5

--- src/main/java/com/droidsam/app/PrimeFactors.java
+++ src/main/java/com/droidsam/app/PrimeFactors.java
@@ -2,6 +2,7 @@ package com.droidsam.app;
 
 public class PrimeFactors {
     public static int[] of(int input) {
+
         if (input == 4) {
             return new int[]{2, 2};
         }
--- src/test/java/com/droidsam/app/PrimeFactorsTest.java
+++ src/test/java/com/droidsam/app/PrimeFactorsTest.java
@@ -14,7 +14,8 @@ public class PrimeFactorsTest {
                 Arguments.of(1, new int[]{1}),
                 Arguments.of(2, new int[]{2}),
                 Arguments.of(3, new int[]{3}),
-                Arguments.of(4, new int[]{2,2})
+                Arguments.of(4, new int[]{2,2}),
+                Arguments.of(5, new int[]{5})
         );
     }
 
