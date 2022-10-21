208754c226dc16dd56033bae1172fdfcf151b660
droidsam1 <droidsam@gmail.com>
2022-10-21 19:49:13 +0200

    refactoring: reduce duplicity by using parametrized test

--- src/test/java/com/droidsam/app/PrimeFactorsTest.java
+++ src/test/java/com/droidsam/app/PrimeFactorsTest.java
@@ -1,23 +1,25 @@
 package com.droidsam.app;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class PrimeFactorsTest {
 
-    @Test
-    public void shouldReturnAnArrayContainingOneWhenInputIsOne(){
-        int input = 1;
-        int [] expectedResult = {1};
-
-        Assertions.assertArrayEquals(expectedResult, PrimeFactors.of(input));
+    public static Stream<Arguments> getPrimeFactorsResultsPairs() {
+        return Stream.of(
+                Arguments.of(1, new int[]{1}),
+                Arguments.of(2, new int[]{2}),
+                Arguments.of(3, new int[]{3})
+        );
     }
 
-    @Test
-    public void shouldReturnAnArrayContainingTwoWhenInputIsTwo(){
-        int input = 2;
-        int [] expectedResult = {2};
-
-        Assertions.assertArrayEquals(expectedResult, PrimeFactors.of(input));
+    @ParameterizedTest
+    @MethodSource("getPrimeFactorsResultsPairs")
+    public void shouldReturnAnArrayContainingOneWhenInputIsOne(int input, int[] expected) {
+        Assertions.assertArrayEquals(expected, PrimeFactors.of(input));
     }
 }
